1330f315bc099133b8aff6e412f7fe3850e4a731
Mohamed Nabil <m.nabil.fci2015@gmail.com>
2021-06-17 19:54:13 +0200

    initial project

---
+++ android/app/src/main/kotlin/com/route/todo_app_mon/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.route.todo_app_mon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
